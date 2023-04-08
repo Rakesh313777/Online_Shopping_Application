d767b2847154926688874916d8189544ee264b1e
your_name_here <103938559+Rakesh313777@users.noreply.github.com>
2023-04-08 11:25:09 +0530

    added swagger

--- Shopping_app_RestAPI_/src/main/java/com/masai/ShoppingAppRestApiApplication.java
+++ Shopping_app_RestAPI_/src/main/java/com/masai/ShoppingAppRestApiApplication.java
@@ -3,7 +3,16 @@ package com.masai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@OpenAPIDefinition(
+		
+		servers = {
+				@Server(url = "/",description = "Default server URL")
+		}
+		)
 
 @SpringBootApplication
 @EnableSwagger2
